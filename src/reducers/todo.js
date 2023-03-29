cfc25f9a8df209a44e15e769f64284fe2b0d03fa
kadam-pallavi <117823209+kadam-pallavi@users.noreply.github.com>
2023-03-29 23:55:40 +0530

    Update todo.js

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,6 +1,8 @@
 const todoReducer = (state, action) => {
     switch (action.type) {
-
+        case 'delTodo': return state.filter(td => td.id !== action.id);
+        case 'addTodo': return [...state,action.obj];
+        default:  return state;
     }
 }
 
